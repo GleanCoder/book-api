d22549fcc68363fab05240d25a8837a7669a692c
GleanCoder <adityakiranacharya0@gmail.com>
2024-10-31 00:22:25 +0530

    routes are defined

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { getAllBooks, getFancyBooks } = require("../controllers/bookController");
 router.route("/").get(getAllBooks);
 router.route("/fancyBooks").get(getFancyBooks);
 module.exports = router;
