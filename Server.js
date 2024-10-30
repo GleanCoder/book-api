7423f4833bcd9ff9215fee4750b5d00f9a8e0013
GleanCoder <adityakiranacharya0@gmail.com>
2024-10-31 00:21:28 +0530

    use middlewere is added for routing

--- Server.js
+++ Server.js
@@ -2,12 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 1172;
+const dbConnection = require("./db/dbConnection");
+const bookRouter = require("./routes/bookRoutes");
 app.get("/", (req, res) => {
   res.send("Welcome to Book API");
 });
-
+app.use("/api/books/", bookRouter);
 async function startServer() {
   try {
+    dbConnection(process.env.DB_URL);
     app.listen(port, () => {
       console.log(`Your server is running at port ${port}`);
     });
